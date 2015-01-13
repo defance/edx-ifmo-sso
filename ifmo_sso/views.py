69985394ae83fd0cd0c9b61a34c2afc800ee4908
Dmitry Ivanyushin <defance@gmail.com>
2015-01-13 16:57:51 +0300

    SSO now serves protocol v. 2

--- ifmo_sso/views.py
+++ ifmo_sso/views.py
@@ -1,15 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib.auth import login, logout as auth_logout
 from django.contrib.auth.models import User
 from django.core.context_processors import csrf
 from django.core.exceptions import SuspiciousOperation
 from django.db import IntegrityError
-from django.http import HttpResponse
 from django.conf import settings
-from django.shortcuts import render, redirect
-from django.template import Context, loader
+from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from edxmako.shortcuts import render_to_response, render_to_string
+from edxmako.shortcuts import render_to_response
 from student.models import UserProfile
 
 # import ifmo_sso
@@ -19,54 +19,66 @@ import logging
 logger = logging.getLogger('ifmo_sso')
 
 
-def sso_login(request, template = 'sso_login.html', redirect_on_success = settings.SSO_REDIRECT):
-    context_dict = { 'csrftoken': csrf(request)['csrf_token'], 'redirect': redirect_on_success }
+def sso_login(request, template='sso_login.html', redirect_on_success=settings.SSO_REDIRECT):
+    context_dict = {'csrftoken': csrf(request)['csrf_token'], 'redirect': redirect_on_success}
     return render_to_response(template, context_dict)
 
 
 @require_POST
 # FIXME: Move secret out of parameters
-def sso_authenticate(request, redirect_on_success = '/', redirect_on_failure = '/login', secret = settings.SSO_SECRET):
+def sso_authenticate(request, redirect_on_success='/', redirect_on_failure='/login', secret=settings.SSO_SECRET):
     u = None
     # FIXME: Handle this user-friedly, now just 500-error is thrown on Exception
     sso_validate_data(request, secret)
 
+    def _get_email():
+        email = request.POST.get('email')
+        if email == '':
+            # FIXME: Move to settings
+            email = u_ssoid + '@niuitmo.ru'
+        return email
+
     try:
         # Try to find user with given SSO ID
         u_ssoid = request.POST.get('ssoid')
-        u = User.objects.get(username__exact = u_ssoid)
+        u = User.objects.get(username__exact=u_ssoid)
+        p = UserProfile.objects.get(user=u)
         # TODO: Update user info concerning incoming data
     except User.DoesNotExist:
         # Register new user
         try:
-            u_email = request.POST.get('email')
-            if u_email == '':
-                # FIXME: Move to settings
-                u_email = u_ssoid + '@niuitmo.ru'
             # Creating User
-            u = User.objects.create_user(username = u_ssoid,
-                                         email = u_email)
-            u.last_name = request.POST.get('lastname')
-            u.first_name = request.POST.get('firstname')
+            u = User.objects.create_user(username=u_ssoid, email=_get_email())
             u.is_active = True
+
             # Creating edX UserProfile
-            # FIXME: Assign user right conserning SSO roles
-            p = UserProfile.objects.create(user_id = u.id)
-            p.name = '%s %s %s' % (u.last_name, u.first_name, request.POST.get('middlename'))
-            # Persisting data
-            u.save()
-            p.save()
+            p = UserProfile.objects.create(user_id=u.id)
+
         except IntegrityError, e:
             # Possible reason: email already exists, FIXME: handle this correctly
-            logger.error ('Failed to register user: Integrity error => ' + e.message)
+            logger.error('Failed to register user: Integrity error => ' + e.message)
             u = None
             p = None
 
+    if p is not None:
+        p.name = '%s %s %s' % (u.last_name, u.first_name, request.POST.get('middlename'))
+        p.gender = request.POST.get('gender')
+        p.year_of_birth = request.POST.get('birthdate').split(' ')[0].split('.')[-1]
+        p.country = request.POST.get('countryCode')
+        # Persisting data
+        p.save()
+
     # If user WAS found or new one registered
     if u is not None:
+        # Update user data
+        u.last_name = request.POST.get('lastname')
+        u.first_name = request.POST.get('firstname')
+        u.email = _get_email()
+        u.save()
         # Dirty hack imitating autheticate(user, hash), see: http://stackoverflow.com/a/2787747
         u.backend = 'django.contrib.auth.backends.ModelBackend'
         login(request, u)
+
     else:
         # Redirect to login page
         # TODO: Use comprehensive messages
@@ -74,7 +86,7 @@ def sso_authenticate(request, redirect_on_success = '/', redirect_on_failure = '
     return redirect(redirect_on_success)
 
 
-def sso_logout(request, redirect_on_complete = settings.SSO_LOGOUTBACK):
+def sso_logout(request, redirect_on_complete=settings.SSO_LOGOUTBACK):
     # Logout from edX
     auth_logout(request)
     # Logout from SSO
@@ -82,18 +94,20 @@ def sso_logout(request, redirect_on_complete = settings.SSO_LOGOUTBACK):
 
 
 @csrf_exempt
-def sso_logout_back(request, redirect_on_complete = '/'):
+def sso_logout_back(request, redirect_on_complete='/'):
     return redirect(redirect_on_complete)
 
 
 # TODO: Move to decorator
 def sso_validate_data(request, secret):
     # TODO: Truncate line
-    str_to_hash = request.POST.get('ssoid') + request.POST.get('lastname') + request.POST.get('firstname') + request.POST.get('middlename') + request.POST.get('birthdate') + request.POST.get('gender') + request.POST.get('roles') + request.POST.get('ttl') + secret 
+    required_fields = [
+        "ssoid", "lastname", "firstname", "middlename", "birthdate", "gender", "countryCode", "roles", "ttl"
+    ]
+    str_to_hash = "".join([request.POST.get(i) for i in required_fields]) + secret
     str_to_hash = str_to_hash.encode('windows-1251')
     hash_result = hashlib.sha1()
     hash_result.update(str_to_hash)
     if request.POST.get('hash') != hash_result.hexdigest().upper():
-        logger.error ('Hash validation failed for ssoid = ' + request.POST.get('ssoid'))
-        logger.info ('SECRET = ' + secret)
+        logger.error('Hash validation failed for ssoid = ' + request.POST.get('ssoid'))
         raise SuspiciousOperation
